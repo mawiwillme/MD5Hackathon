a0e284c0a301988607b54fd2e27432569856680d
mark-wilmes <mawi0949@colorado.edu>
2018-04-07 10:08:06 -0600

    requirements for python file, and good pcap

--- anomolyDetection.py
+++ anomolyDetection.py
@@ -3,9 +3,18 @@ import pyshark
 #from sklearn import *
 
 def sanitizeData():
+    counter = 0
+    total = 0
     cap = pyshark.FileCapture('mstp_2018_Bad1.cap')
     for packet in cap:
-        print(packet)
+        total += 1
+        layer = packet.mstp
+        frameType = layer.frame_type
+        if(frameType == '1'):
+            print("HERE")
+            counter += 1
+    print(counter)
+    print(total)
     print("Sanitizing")
     # Sanitizes the data so it can be passed into a scikit leanr classifier
 
