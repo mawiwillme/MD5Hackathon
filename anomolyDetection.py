dc4873f04dcaa0490359a323328a442cc6fba09f
mark-wilmes <mawi0949@colorado.edu>
2018-04-07 13:23:20 -0600

    Distance metric added to the anomolyDetection

--- anomolyDetection.py
+++ anomolyDetection.py
@@ -1,8 +1,17 @@
 
 #import pyshark
-from sklearn import *
+from sklearn import linear_model
+from sklearn import neighbors
 import matplotlib.pylab as plt
 import numpy as np
+
+def regressionOnData(x,y):
+    dist = neighbors.DistanceMetric.get_metric('euclidean')
+
+    data = []
+    for i in range(len(x)):
+        data.append(np.array([x[i],y[i]]))
+    coefs = dist.pairwise(data)
 
 def y_noisy(a,b,x,var):
     noise = np.random.normal(0,var,len(x))
@@ -10,8 +19,12 @@ def y_noisy(a,b,x,var):
 
 def simulateVotes(var=0.05,alpha=0.5,beta=0,n_points=20):
     alpha2 = np.random.choice([0.25,0.75])    
-    x = np.linspace(0,20,n_points)
-    y = y_noisy(alpha2,beta,x,var)
+    x = np.array(np.linspace(0,20,n_points))
+    y = np.array(y_noisy(alpha2,beta,x,var))
+
+    regressionOnData(x,y)
+    regressionOnData
+    '''
     fig, ax = plt.subplots()
     scatter = ax.scatter(x,y,marker='o',label="y=x+0.5+noise")
     line = ax.plot(x,beta*x+alpha,label="y=x+0.5")
@@ -19,7 +32,7 @@ def simulateVotes(var=0.05,alpha=0.5,beta=0,n_points=20):
     ax.set_ylabel("y", fontsize=16)
     plt.legend()
     plt.show()
-
+    '''
     
 
 def classifier():
