8cc7df6c99c94335ed05734436b5ea4c356153b2
mark-wilmes <mawi0949@colorado.edu>
2018-04-07 12:52:51 -0600

    Plot that plots test data

--- anomolyDetection.py
+++ anomolyDetection.py
@@ -1,25 +1,29 @@
 
-import pyshark
-#from sklearn import *
+#import pyshark
+from sklearn import *
+import matplotlib.pylab as plt
+import numpy as np
 
-def sanitizeData():
-    counter = 0
-    total = 0
-    cap = pyshark.FileCapture('mstp_2018_Bad1.cap')
-    for packet in cap:
-        total += 1
-        layer = packet.mstp
-        frameType = layer.frame_type
-        if(frameType == '1'):
-            print("HERE")
-            counter += 1
-    print(counter)
-    print(total)
-    print("Sanitizing")
-    # Sanitizes the data so it can be passed into a scikit leanr classifier
+def y_noisy(a,b,x,var):
+    noise = np.random.normal(0,var,len(x))
+    return a+x*b+noise
+
+def simulateVotes(var=0.05,alpha=0.5,beta=0,n_points=20):
+    alpha2 = np.random.choice([0.25,0.75])    
+    x = np.linspace(0,20,n_points)
+    y = y_noisy(alpha2,beta,x,var)
+    fig, ax = plt.subplots()
+    scatter = ax.scatter(x,y,marker='o',label="y=x+0.5+noise")
+    line = ax.plot(x,beta*x+alpha,label="y=x+0.5")
+    ax.set_xlabel("x", fontsize=16)
+    ax.set_ylabel("y", fontsize=16)
+    plt.legend()
+    plt.show()
+
+    
 
 def classifier():
     print("classifying")
     # Runs scikit leanr classifier to learn patterns in data sets
 
-sanitizeData()
+simulateVotes()
