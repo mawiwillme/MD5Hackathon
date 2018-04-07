7ed218a00acd497dac6738195487437228dff267
mark-wilmes <mawi0949@colorado.edu>
2018-04-07 09:04:31 -0600

    Added python file for anomoly detection

---
+++ anomolyDetection.py
@@ -0,0 +1,9 @@
+from sklearn import *
+
+def sanitizeData():
+    print("Sanitizing")
+    # Sanitizes the data so it can be passed into a scikit leanr classifier
+
+def classifier():
+    print("classifying")
+    # Runs scikit leanr classifier to learn patterns in data sets
